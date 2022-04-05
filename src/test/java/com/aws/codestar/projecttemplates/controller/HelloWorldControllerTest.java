49d75e83e3d91db9621651eff9744cbf56d69aa4
venkata-alt <raghavendraraoadapala@gmail.com>
2022-04-05 13:57:40 +0530

    changes done

--- src/test/java/com/aws/codestar/projecttemplates/controller/HelloWorldControllerTest.java
+++ src/test/java/com/aws/codestar/projecttemplates/controller/HelloWorldControllerTest.java
@@ -16,7 +16,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 @DisplayName("Tests for HelloWorldController")
 class HelloWorldControllerTest {
 
-    private static final String EXPECTED_RESPONSE_VALUE = "HELLO AWS CodeStar!";
+    private static final String EXPECTED_RESPONSE_VALUE = "welcome to my world AWS CodeStar!";
     private static final String INPUT_NAME = "AWS CodeStar";
 
     private final HelloWorldController controller = new HelloWorldController();
